712fd66c8c923154a2e0ff6c8e5d06b10452e00b
clockwerkz <clockwerkz@gmail.com>
2020-05-26 13:11:44 -0700

    refactoring project to closer match vrms

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# accessControl
+# Member Roles and Permissions
 MERN Stack project that incorporates OAuth and Controlled Access depending on user's assigned level.
+
+
+## Role Definitions
+Role types - Project members, Project Managers, and Project Admins. 3 different permission levels, but requires scalability to add more roles and responsibilities.
+
+- Project Members: can clock in their time, and view the roster
+
+- Project Managers: view/edit roster, limited editing of the project
+
+- Project Admins: Full read/write of the project
+
+### Notes
+
+Members are a potential many to many(few) relationship with projects.. projects will have many members, members may be a part of one or more..
+<br>
+Project Managers - few to many - Each project may have 1 or more project managers.
+<br>
+Because there various projects, roles must also state what project they belong to:
+<br>
+<code>
+    User {
+        affiliations: [ '1:member' ]   //projectID:role
+    }
+</code>
--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,7 @@ import { userLevels, permissionLevels } from './utilites.js/authorize';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import AccessControl from './components/AccessControl';
-import StatsPanel from './components/StatsPanel';
-import ReactorControls from './components/ReactorControls';
-import EmergencyCommunications from './components/EmergencyCommunications';
+
 import NoAccess from './components/NoAccess';
 
 
@@ -23,33 +21,11 @@ function App() {
 
   const handleLogout = () => setUser('');
   return (
-    <div className="App">
-        <h1>Reactor Access Control</h1>
-        {!user ? (<Login handleLogin={handleLogin}/>) 
-        : 
-        (<Dashboard user={user} userLevels={userLevels[user]} handleLogout={handleLogout}/>)}
-        <AccessControl 
-          userPermissions={userPermissions}
-          allowedPermissions={permissionLevels.StatsPanel}
-          renderNoAccess={()=>renderNoAccess(permissionLevels.StatsPanel)}
-        >
-          <StatsPanel />
-        </AccessControl>
-        <AccessControl 
-          userPermissions={userPermissions}
-          allowedPermissions={permissionLevels.EmergencyCommunications}
-          renderNoAccess={()=>renderNoAccess(permissionLevels.EmergencyCommunications)}
-        >
-          < EmergencyCommunications />
-        </AccessControl>
-        <AccessControl 
-          userPermissions={userPermissions}
-          allowedPermissions={permissionLevels.ReactorControls}
-          renderNoAccess={()=>renderNoAccess(permissionLevels.ReactorControls)}
-        >
-          <ReactorControls />
-        </AccessControl>
-    </div>
+    <BrowserRouter>
+      <Route path="/dashboard">
+        <Dashboard />
+      </Route>
+    </BrowserRouter>
   );
 }
 
