28fa054a6974452c5ca558048d075d33b5c3e8ce
Carlos <clockwerkz@gmail.com>
2020-06-08 14:13:11 -0700

    following auth0 demo - building basic web app auth

--- server.js
+++ server.js
@@ -1,46 +1,83 @@
 const express = require('express');
 const app = express();
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
 
 const PORT = process.env.PORT || 5000;
 
-// Set up Auth0 configuration
-const authConfig = {
-    domain: "YOUR_DOMAIN",
-    audience: "YOUR_API_IDENTIFIER"
+var session = require('express-session');
+
+// config express-session
+var sess = {
+  secret: 'SOME ORT OF SCERTEADSADAS',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
 };
 
-const checkJwt = jwt({
-    secret: jwksRsa.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: 'https://dev-64n7s4br.auth0.com/.well-known/jwks.json'
-  }),
-  audience: 'http://localhost:5000/',
-  issuer: 'https://dev-64n7s4br.auth0.com/',
-  algorithms: ['RS256']
-});
+if (app.get('env') === 'production') {
+  // Use secure cookies in production (requires SSL/TLS)
+  sess.cookie.secure = true;
+
+  // Uncomment the line below if your application is behind a proxy (like on Heroku)
+  // or if you're encountering the error message:
+  // "Unable to verify authorization request state"
+  // app.set('trust proxy', 1);
+}
+
+app.set("view engine", "pug");
+
+app.use(session(sess));
+
+// Load environment variables from .env
+var dotenv = require('dotenv');
+dotenv.config();
+
+// Load Passport
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
+
+// Configure Passport to use Auth0
+var strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:5000/callback'
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// You can use this section to keep a smaller payload
+passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
   
+  passport.deserializeUser(function (user, done) {
+    done(null, user);
+  });
+
+var userInViews = require('./lib/middleware/userInViews');
+var authRouter = require('./routes/auth');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 
-
-app.get("/api/external", checkJwt, (req, res) => {
-    console.log('route hit');
-    res.send({
-      msg: "Your Access Token was successfully validated!"
-    });
-});
+app.use(userInViews());
+app.use('/', authRouter);
+app.use('/', indexRouter);
+app.use('/', usersRouter);
 
 
-app.get('/test', (req, res)=> {
-    console.log('route hit');
-    msg = {
-        msg: "This is from the back end!"
-    }
-    return res.status(200).json(msg);
-});
 
 app.listen(PORT, ()=> {
     console.log(`Server listening on ${PORT}...`);
